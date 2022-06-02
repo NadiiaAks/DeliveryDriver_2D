146fd5fec80c051c099fc4d230d2ff1f45d7a276
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-02 21:31:05 +0300

    Add Collision

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        Debug.Log("Bumb!");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Debug.Log("Is trigger!");
     }
 }
