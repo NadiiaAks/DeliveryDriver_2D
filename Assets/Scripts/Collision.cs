28266910db0bf552e40e20cda0b2b009489d7b48
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-03 21:28:44 +0300

    Add delivery logic

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    private bool _hasPackage;
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Bumb!");
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Is trigger!");
+        if(other.tag == "Package")
+        {
+            Debug.Log("It's package");
+            _hasPackage = true;
+        }
+
+        if(other.tag == "Customer" && _hasPackage)
+        {
+            Debug.Log("Delivered!");
+            _hasPackage = false;
+        }
     }
 }
